bf7b872ed0728f7625e966781b9895de88700905
ssabii <fhy1930@gmail.com>
2023-01-01 11:55:58 +0900

    feat: exportInputSizes

--- src/components/common/TextField/TextField.tsx
+++ src/components/common/TextField/TextField.tsx
@@ -3,8 +3,7 @@ import styled from '@emotion/styled';
 import { WithRequired } from 'utils/WithRequired';
 import { css, keyframes } from '@emotion/react';
 import colors from 'styles/colors';
-
-export type InputSizes = 'xsmall' | 'small' | 'medium' | 'large' | 'xlarge'
+import { InputSizes } from '../InputSizes';
 
 export type InputAlign = 'inherit' | 'left' | 'center' | 'right' | 'justify';
 

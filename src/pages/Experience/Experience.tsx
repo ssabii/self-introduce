648f7178a82e76e843a0f552ed345f686310e8cc
ssabii <fhy1930@gmail.com>
2023-01-03 21:48:59 +0900

    feat: Experience 페이지 구현

---
+++ src/pages/Experience/Experience.tsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import styled from '@emotion/styled';
+import PageContainer from 'components/common/PageContainer'
+import InView from 'components/common/InView';
+import UnityLogo from './assets/unity.png';
+import ReactLogo from './assets/react.png';
+
+function Experience() {
+  return (
+    <PageContainer>
+      <Contents>
+        <InView delay={1000}>
+          <img alt="유니티" src={UnityLogo} width={450} />
+        </InView>
+        <InView delay={2000}>
+          <Emoji>➡</Emoji>
+        </InView>
+        <InView delay={3000}>
+          <img alt="리액트" src={ReactLogo} width={450} />
+        </InView>
+      </Contents>
+    </PageContainer >
+  )
+};
+
+const Contents = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  gap: 50px;
+`;
+
+const Emoji = styled.div`
+  font-size: 80px;
+  text-align: center;
+`
+
+export default Experience;
+
--- src/routes/MainRouter.tsx
+++ src/routes/MainRouter.tsx
@@ -3,6 +3,7 @@ import { Routes, Route } from 'react-router-dom'
 import Layout from 'components/layouts/Layout'
 import Main from 'pages/Main';
 import Intro from 'pages/Intro';
+import Experience from 'pages/Experience';
 
 function MainRouter() {
   return (
@@ -10,6 +11,7 @@ function MainRouter() {
       <Route path="/" element={<Layout />}>
         <Route index element={<Main />} />
         <Route path="intro" element={<Intro />} />
+        <Route path="experience" element={<Experience />} />
       </Route>
     </Routes>
   )

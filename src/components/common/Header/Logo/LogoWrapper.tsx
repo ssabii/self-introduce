3cafd37a69d90bb224f84680c48eef9c79522761
ssabii <fhy1930@gmail.com>
2023-01-04 15:20:43 +0900

    fix: 로고 링크 주소 수정

--- src/components/common/Header/Logo/LogoWrapper.tsx
+++ src/components/common/Header/Logo/LogoWrapper.tsx
@@ -2,10 +2,11 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import styled from '@emotion/styled'
 import Logo from './Logo'
+import { routePrefix } from 'routes/routePrefix'
 
 function LogoWrapper() {
   return (
-    <StyledLink to="/" aria-label='logo'>
+    <StyledLink to={routePrefix} aria-label='logo'>
       <Logo width={85} />
     </StyledLink>
   )
